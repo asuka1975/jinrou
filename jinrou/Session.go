f16cff1c91a50e6a3ded543244f2932ae19a2ff8
hungr <shikina.asuka@i.mbox.nagoya-u.ac.jp>
2021-02-12 00:17:55 +0900

    define the design of ISession

--- jinrou/Session.go
+++ jinrou/Session.go
@@ -2,7 +2,10 @@ package jinrou
 
 type ISession interface {
 	Next() ISession
-	String() string
+	PushCommand(queue CommandQueue)
+	End()
+	Interactive() bool
+	GetID() SessionID
 }
 
 type SessionID int
@@ -13,3 +16,95 @@ const (
 	Evening SessionID = 4
 	Night   SessionID = 8
 )
+
+type MorningSession struct {
+}
+
+func (s MorningSession) Next() ISession {
+	return &NoonSession{}
+}
+
+func (s MorningSession) PushCommand(queue CommandQueue) {
+
+}
+
+func (s MorningSession) End() {
+
+}
+
+func (s MorningSession) Interactive() bool {
+	return false
+}
+
+func (s MorningSession) GetID() SessionID {
+	return Morning
+}
+
+type NoonSession struct {
+}
+
+func (s NoonSession) Next() ISession {
+	return &EveningSession{}
+}
+
+func (s NoonSession) PushCommand(queue CommandQueue) {
+
+}
+
+func (s NoonSession) End() {
+
+}
+
+func (s NoonSession) Interactive() bool {
+	return true
+}
+
+func (s NoonSession) GetID() SessionID {
+	return Noon
+}
+
+type EveningSession struct {
+}
+
+func (s EveningSession) Next() ISession {
+	return &NightSession{}
+}
+
+func (s EveningSession) PushCommand(queue CommandQueue) {
+
+}
+
+func (s EveningSession) End() {
+
+}
+
+func (s EveningSession) Interactive() bool {
+	return false
+}
+
+func (s EveningSession) GetID() SessionID {
+	return Evening
+}
+
+type NightSession struct {
+}
+
+func (s NightSession) Next() ISession {
+	return &MorningSession{}
+}
+
+func (s NightSession) PushCommand(queue CommandQueue) {
+
+}
+
+func (s NightSession) End() {
+
+}
+
+func (s NightSession) Interactive() bool {
+	return false
+}
+
+func (s NightSession) GetID() SessionID {
+	return Night
+}
